7eca24253a884898ceaee667d6639608849f1c7c
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2024-11-07 20:33:36 +0530

    error handling

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/AppError.js";
+import jwt from "jsonwebtoken";
+import { HttpStatusCode } from "../constants/httpStatusCode.enum.js";
+import { ConsoleLog } from "../utils/ConsoleLog.js";
 import {
-    sendResponseClientError,
-    sendResponseServerError
-} from "../utils/sendResponse.js";
+    AuthenticationError,
+    AuthorizationError,
+    ClientError,
+    ValidationError
+} from "../utils/AppErrors.js";
+const { TokenExpiredError } = jwt;
 
 export const errorHandler = (
     err: Error | AppError,
@@ -12,13 +18,57 @@ export const errorHandler = (
     next: NextFunction
 ) => {
     try {
+        ConsoleLog.error(err);
         if (err instanceof AppError) {
-            sendResponseClientError(res, err.statusCode, err.message);
-        } else if (err instanceof Error) {
-            sendResponseClientError(res, err.message);
+            res.status(err.statusCode).json({
+                success: false,
+                message: err.message,
+                errorCode: "SOME_ERROR"
+            });
+        } else if (err instanceof ValidationError) {
+            res.status(err.statusCode).json({
+                success: false,
+                message: err.message,
+                errorCode: err.errorCode,
+                errors: err.errors
+            });
+        } else if (err instanceof TokenExpiredError) {
+            res.status(HttpStatusCode.UNAUTHORIZED).json({
+                success: false,
+                message: "Session expired. Please login again.",
+                errorCode: "TOKEN_EXPIRED"
+            });
+        } else if (err instanceof AuthenticationError) {
+            res.status(err.statusCode).json({
+                success: false,
+                message: err.message,
+                errorCode: err.errorCode
+            });
+        } else if (err instanceof AuthorizationError) {
+            res.status(err.statusCode).json({
+                success: false,
+                message: err.message,
+                errorCode: err.errorCode
+            });
+        } else if (err instanceof ClientError) {
+            res.status(err.statusCode).json({
+                success: false,
+                message: err.message,
+                errorCode: "CLIENT_ERROR"
+            });
+        } else {
+            res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                message: "Internal server error",
+                errorCode: "INTERNAL_SERVER_ERROR"
+            });
         }
     } catch (error) {
-        console.log(error);
-        sendResponseServerError(res);
+        ConsoleLog.error(error);
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Internal server error",
+            errorCode: "INTERNAL_SERVER_ERROR"
+        });
     }
 };
