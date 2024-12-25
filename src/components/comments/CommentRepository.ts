c3d044fa0ffcc9b0f6809b2ca794530347db2acf
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2024-12-25 18:55:11 +0530

    comments section

---
+++ src/components/comments/CommentRepository.ts
@@ -0,0 +1,56 @@
+import { Reaction } from "../../constants/CommentReaction.js";
+import { Comment, NewComment, NewReply } from "./Comment.js";
+import { CommentReaction } from "./CommentReaction.js";
+import { Sort } from "./CommentService.js";
+
+export interface CommentRepository {
+    addComment(comment: NewComment): Promise<Comment | null>;
+    getCommentsByGameId(
+        gameId: string,
+        page: number,
+        limit: number,
+        sort: Sort
+    ): Promise<Partial<Comment>[]>;
+    getCommentsByUserId(userId: string): Promise<Comment[]>;
+    updateComment(
+        commentId: string,
+        comment: Partial<Comment>
+    ): Promise<boolean>;
+    increamentCount(
+        commentId: string,
+        likeCount: number,
+        dislikeCount: number,
+        replyCount: number,
+        activityCount: number
+    ): Promise<boolean>;
+    deleteComment(commentId: string): Promise<boolean>;
+    addReaction(
+        commentId: string,
+        userId: string,
+        reaction: Reaction
+    ): Promise<boolean>;
+    removeReaction(
+        commentId: string,
+        userId: string,
+        reaction: Reaction
+    ): Promise<boolean>;
+    updateReaction(
+        commentId: string,
+        userId: string,
+        reaction: Reaction
+    ): Promise<boolean>;
+    getMyReaction(
+        commentId: string,
+        userId: string
+    ): Promise<CommentReaction | null>;
+    getMyReactions(
+        commentIdList: string[],
+        userId: string
+    ): Promise<CommentReaction[] | null>;
+    addReply(reply: NewReply): Promise<Comment | null>;
+    getRepliesByCommentId(
+        commentId: string,
+        page: number,
+        limit: number
+    ): Promise<Partial<Comment>[]>;
+}
