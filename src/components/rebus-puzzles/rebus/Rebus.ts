02c71cb6df69f393b73026403c66e742b39f82e8
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-06-02 21:45:41 +0530

    Rebus

---
+++ src/components/rebus-puzzles/rebus/Rebus.ts
@@ -0,0 +1,13 @@
+import { Difficulty } from "../../../constants/Difficulty.js";
+import { User } from "../../users/User.js";
+
+export interface Rebus {
+    rebusId: string;
+    rebusImageUrl: string;
+    answer: string;
+    difficulty: Difficulty;
+    explanation?: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    addedBy: User["userId"] | Partial<User>;
+}
