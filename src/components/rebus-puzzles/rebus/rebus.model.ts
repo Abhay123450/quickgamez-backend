fffe019bd4880a85b203b8bd2b7d07bc2ba26ba1
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-06-04 23:49:06 +0530

    rebus model

---
+++ src/components/rebus-puzzles/rebus/rebus.model.ts
@@ -0,0 +1,45 @@
+import { Document, model, Schema } from "mongoose";
+import { Rebus } from "./Rebus.js";
+import { Difficulty } from "../../../constants/Difficulty.js";
+
+export interface RebusDocument
+    extends Omit<Rebus, "rebusId" | "addedBy">,
+        Document {
+    addedBy: Schema.Types.ObjectId;
+}
+
+export const rebusSchema = new Schema<RebusDocument>(
+    {
+        rebusImageUrl: {
+            type: String,
+            required: true
+        },
+        answer: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        difficulty: {
+            type: String,
+            enum: Difficulty,
+            required: true,
+            lowercase: true,
+            trim: true
+        },
+        explanation: {
+            type: String,
+            trim: true,
+            default: null
+        },
+        addedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const RebusModel = model<RebusDocument>("Rebus", rebusSchema, "rebus");
