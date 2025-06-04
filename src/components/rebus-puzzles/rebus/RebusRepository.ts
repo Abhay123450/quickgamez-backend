f77be33404e14b6cfc4672c74e874cf78acbd693
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-06-04 23:46:27 +0530

    rebus repository interface

---
+++ src/components/rebus-puzzles/rebus/RebusRepository.ts
@@ -0,0 +1,26 @@
+import { User } from "../../users/User.js";
+import { Rebus } from "./Rebus.js";
+
+export interface RebusRepository {
+    addRebus(
+        rebus: Omit<Rebus, "rebusId" | "createdAt" | "updatedAt">
+    ): Promise<Rebus>;
+    addRebusMany(
+        rebuses: Omit<Rebus, "rebusId" | "createdAt" | "updatedAt">[]
+    ): Promise<Rebus[]>;
+    getRebusById(rebusId: Rebus["rebusId"]): Promise<Rebus>;
+    getRebus(
+        filter: Partial<Rebus>,
+        page?: number,
+        count?: number
+    ): Promise<Rebus[]>;
+    getRandomRebus(
+        count: number,
+        difficulty?: Rebus["difficulty"]
+    ): Promise<Rebus[]>;
+    getUnplayedRebus(
+        userId: User["userId"],
+        count: number,
+        difficulty?: Rebus["difficulty"]
+    ): Promise<Rebus[]>;
+}
